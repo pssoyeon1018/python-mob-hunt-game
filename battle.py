6c97e9859e9f6072660a52c3ff8c837a6d832813
soyeon <pssoyeon.sesac@gmail.com>
2026-07-02 14:25:47 +0900

    battle 페이지 구성

--- battle.py
+++ battle.py
@@ -1,1 +1,17 @@
-print("전투를 시작합니다!")
+class BattleManager:
+    def is_monster_dead(self, monster):
+        """
+        몬스터의 HP가 0 이하인지 확인하여 사망 여부를 반환합니다.
+        """
+        return monster.hp <= 0
+
+    def player_attack(self, player, monster):
+        """
+        플레이어가 몬스터를 공격하고 결과를 출력합니다.
+        """
+        player.attack(monster)
+        
+        if self.is_monster_dead(monster):
+            print(f"[{monster.name}]을(를) 처치했습니다!")
+        else:
+            print(f"[{monster.name}]의 남은 HP: {monster.hp}")
